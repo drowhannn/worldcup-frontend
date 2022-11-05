553e717059f85f3972f47bc9c7fcc50b5747c317
Rohan Dhimal <drowhann@gmail.com>
2022-11-05 09:36:44 +0545

    fix: minor fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
-    <div class="w-full overflow-hidden relative">
-        <img src="/images/banner.avif" class="object-cover w-full max-h-[512px]" alt="banner">
-        <div class="w-full h-full absolute top-0 opacity-50 bg-gradient-to-b from-black to-gray-700"></div>
-        <div class="absolute top-1/4 sm:top-1/3 p-3 md:p-6">
-            <div class="text-3xl md:text-6xl lg:text-8xl font-bold text-gray-300">
-                <p>QATAR FIFA</p>
-                <p>WORLD CUP 2022</p>
-            </div>
-        </div>
+    <div class="w-screen h-screen">
+        <Banner />
     </div>
 </template>
